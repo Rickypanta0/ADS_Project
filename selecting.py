dc97fa84c10565ebc8022a63101e3855cc3e994e
Ludovico Gerardi <gerardiludo@gmail.com>
2024-04-13 09:39:54 +0200

    [selecting] Refactoring, diciamo

--- selecting.py
+++ selecting.py
@@ -1,7 +1,8 @@
-from sorting import *
-from Min_Max_heap import *
-import math
+from math import ceil
 from functools import partial
+
+from sorting import partition, insertionsort
+from Min_Max_heap import MinHeap, MaxHeap
 
 
 def heap_select(A, k):
@@ -16,8 +17,8 @@ def heap_select(A, k):
         H2 = MaxHeap()
     H2.Heap_Insert(H1.A[0])
 
-    for i in range(k-1):
-        root = H2.Heap_Extract()
+    for i in range(k - 1):
+        H2.Heap_Extract()
         left = H1.left(i)
         right = H1.right(i)
         if left <= H1.heapsize:
@@ -39,18 +40,18 @@ def median_of_medians_non_in_place(A, *, start=0, end=None, blocksize=5):
             A      : the list to calculate the median of.
         Keyword arguments:
             start    : the starting point of the subarray; defaults to 0;
-            end      : the ending point (NOT included) of the subarray; defaults
-                        to the length of A;
-            blocksize: the size of the blocks of elements sorted; defaults to 5.
-
+            end      : the ending point (NOT included) of the subarray;
+                        defaults to the length of A;
+            blocksize: the size of the blocks of elements sorted; default
+                        to 5.
 
         Return:
             the median of medians.
     """
 
-    # Per ogni blocco b di dimensione blocksize, trova la mediana ordinandolo e
-    # aggiungi la mediana a una nuova lista (medians). Effettua la ricorsione su
-    # quella lista finché non rimane solo un elemento, cioè la mediana delle
+    # Per ogni blocco di dimensione blocksize, trova la mediana ordinandolo e
+    # aggiungi la mediana a una nuova lista (medians). Effettua la ricorsione
+    # su quella lista finché non rimane solo un elemento, cioè la mediana delle
     # mediane.
 
     if end is None:
@@ -63,10 +64,10 @@ def median_of_medians_non_in_place(A, *, start=0, end=None, blocksize=5):
     medians = []
 
     # Il numero di mediane da calcolare è dato dal rapporto tra il numero di
-    # elementi del sottovettore considerato e la dimensione dei blocchi. Siccome
-    # l'ultimo blocco può avere dimensione strettamente minore di blocksize, si
-    # arrotonda per eccesso.
-    n_medians = math.ceil(subarray_length / blocksize)
+    # elementi del sottovettore considerato e la dimensione dei blocchi.
+    # Siccome l'ultimo blocco può avere dimensione strettamente minore di
+    # blocksize, si arrotonda per eccesso.
+    n_medians = ceil(subarray_length / blocksize)
     for i in range(n_medians):
         block_start = start + i * blocksize
         block_end = min(block_start + blocksize, end)
@@ -89,18 +90,20 @@ def median_of_medians_quasi_in_place(A, *, start=0, end=None, blocksize=5):
             A      : the list to calculate the median of.
         Keyword arguments:
             start    : the starting point of the subarray; defaults to 0;
-            end      : the ending point (NOT included) of the subarray; defaults
-                        to the length of A;
-            blocksize: the size of the blocks of elements sorted; defaults to 5.
+            end      : the ending point (NOT included) of the subarray;
+                        defaults to the length of A;
+            blocksize: the size of the blocks of elements sorted; defaults
+                        to 5.
 
         Return:
             the position of the median of medians.
     """
 
-    # Per ogni blocco b di dimensione blocksize, trova la mediana ordinandolo e
-    # sposta la mediana all'inizio del sottovettore. Effettua la ricorsione su
-    # sulla parte iniziale del sottovettore, finché il sottovettore considerato
-    # ha dimensione 1, cioè quando contiene solo la mediana delle mediane.
+    # Per ogni blocco di dimensione blocksize, trova la mediana ordinandolo e
+    # sposta la mediana verso l'inizio del sottovettore. Effettua la ricorsione
+    # su sulla parte iniziale del sottovettore finché il sottovettore
+    # considerato non ha dimensione 1, cioè quando contiene solo la mediana
+    # delle mediane.
 
     if end is None:
         end = len(A)
@@ -110,24 +113,24 @@ def median_of_medians_quasi_in_place(A, *, start=0, end=None, blocksize=5):
         return start
 
     # Il numero di mediane da calcolare è dato dal rapporto tra il numero di
-    # elementi del sottovettore considerato e la dimensione dei blocchi. Siccome
-    # l'ultimo blocco può avere dimensione strettamente minore di blocksize, si
-    # arrotonda per eccesso.
-    n_medians = math.ceil(subarray_length / blocksize)
+    # elementi del sottovettore considerato e la dimensione dei blocchi.
+    # Siccome l'ultimo blocco può avere dimensione strettamente minore di
+    # blocksize, si arrotonda per eccesso.
+    n_medians = ceil(subarray_length / blocksize)
     for i in range(n_medians):
         block_start = start + i * blocksize
         block_end = min(block_start + blocksize, end)
         insertionsort(A, start=block_start, end=block_end)
         median_position = (block_start + block_end) // 2
-        A[start+i], A[median_position] = A[median_position], A[start+i]
+        A[start + i], A[median_position] = A[median_position], A[start + i]
 
     # Dato che le mediane vengono sempre spostate nella parte iniziale del
-    # sottovettore start rimane inalterato; invece end diventa start+n_medians
-    # perché sono state spostate esattamente n_medians mediane e quindi la parte
-    # iniziale contenente le mediane è lunga n_medians.
-    return median_of_medians_quasi_in_place(A, start=start,
-                                            end=start+n_medians,
-                                            blocksize=blocksize)
+    # sottovettore, start rimane inalterato; invece end diventa start+n_medians
+    # perché sono state spostate esattamente n_medians mediane e quindi la
+    # parte iniziale contenente le mediane è lunga n_medians.
+    return median_of_medians_quasi_in_place(
+        A, start=start, end=start + n_medians, blocksize=blocksize
+    )
 
 
 def median_of_medians_in_place(A, *, start=0, end=None, blocksize=5):
@@ -142,9 +145,10 @@ def median_of_medians_in_place(A, *, start=0, end=None, blocksize=5):
             A      : the list to calculate the median of.
         Keyword arguments:
             start    : the starting point of the subarray; defaults to 0;
-            end      : the ending point (NOT included) of the subarray; defaults
-                        to the length of A;
-            blocksize: the size of the blocks of elements sorted; defaults to 5.
+            end      : the ending point (NOT included) of the subarray;
+                        defaults to the length of A;
+            blocksize: the size of the blocks of elements sorted; defaults
+                        to 5.
 
         Return:
             the position of the median of medians.
@@ -156,21 +160,21 @@ def median_of_medians_in_place(A, *, start=0, end=None, blocksize=5):
         end = len(A)
 
     while (subarray_length := end - start) > 1:
-        # Il numero di mediane da calcolare è dato dal rapporto tra il numero di
-        # elementi del sottovettore considerato e la dimensione dei blocchi. Siccome
-        # l'ultimo blocco può avere dimensione strettamente minore di blocksize, si
-        # arrotonda per eccesso.
-        n_medians = math.ceil(subarray_length / blocksize)
+        # Il numero di mediane da calcolare è dato dal rapporto tra il numero
+        # di elementi del sottovettore considerato e la dimensione dei blocchi.
+        # Siccome l'ultimo blocco può avere dimensione strettamente minore di
+        # blocksize, si arrotonda per eccesso.
+        n_medians = ceil(subarray_length / blocksize)
         for i in range(n_medians):
             block_start = start + i * blocksize
             block_end = min(block_start + blocksize, end)
             insertionsort(A, start=block_start, end=block_end)
             median_position = (block_start + block_end) // 2
-            A[start+i], A[median_position] = A[median_position], A[start+i]
-        end = start + n_medians # vedi commento nella versione quasi in-place
+            A[start + i], A[median_position] = A[median_position], A[start + i]
+        end = start + n_medians  # vedi commento nella versione quasi in-place
 
-    assert subarray_length == 1 # guardia nel caso in cui il ciclo dovesse
-                                # finire con subarray_length < 0
+    assert subarray_length == 1  # guardia nel caso in cui il ciclo dovesse
+    # finire con subarray_length < 0
     return start
 
 
@@ -179,19 +183,20 @@ def select(A, k, *, start=0, end=None, pfunc):
     assert 0 <= k < len(A)
     assert 0 <= start < len(A)
     assert end is None or start < end <= len(A)
+    assert pfunc is not None
 
     """Select the kth smallest element in list A.
 
         Positional arguments:
-            A     : the list to search;
+            A       : the list to search;
             k       : the position of the element to select.
         Keyword arguments:
             start   : the starting point of the portion of A to consider;
                         defaults to 0;
             end     : the ending point (NOT included) of the portion of A to
-                        consider; defaults to len(A);
-            pfunc   : a function which returns the position of the pivot for the
-                        partitioning; this function must accept two keyword
+                        consider; defaults to the length of A;
+            pfunc   : a function which returns the position of the pivot for
+                        the partitioning; this function must accept two keyword
                         arguments: start and end.
         Return:
             the selected element.
@@ -208,13 +213,14 @@ def select(A, k, *, start=0, end=None, pfunc):
     elif p > k:
         return select(A, k, start=start, end=p, pfunc=pfunc)
     else:
-        return select(A, k, start=p+1, end=end, pfunc=pfunc)
+        return select(A, k, start=p + 1, end=end, pfunc=pfunc)
 
 
 # funzione "fantoccio" che ritorna sempre end-1 (ultima posizione della lista)
 # come posizione del pivot
 def _standard_pfunc(A, *, start, end):
     return end - 1
+
 
 # quick select si ottiene usando _standard_pfunc come funzione per trovare il
 # pivot
